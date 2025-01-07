c37eab9dd09a32cc43130276bb293a39bbc5a4c3
sajakshrestha8 <shresthasajak123@gmail.com>
2025-01-07 21:57:59 +0545

    Update

--- RandomSpiders/js/main.js
+++ RandomSpiders/js/main.js
@@ -3,27 +3,73 @@ import Spider from "./spider.js";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let mouseDirection = {
+  x: 0,
+  y: 0,
+};
+
 const spider = new Spider(
   Math.random() * canvas.width - 10,
   Math.random() * canvas.height - 10
 );
 
 let spiderHouse = Array.from(
-  { length: 100 },
+  { length: 10 },
   () =>
     new Spider(
       Math.random() * (canvas.width - 20),
-      Math.random() * (canvas.height - 20)
+      Math.random() * (canvas.height - 20),
+      30,
+      30
     )
 );
 
+const backGroundImage = new Image();
+backGroundImage.src =
+  "https://images.pexels.com/photos/129733/pexels-photo-129733.jpeg?cs=srgb&dl=pexels-fwstudio-33348-129733.jpg&fm=jpg";
+
 function gameLoop() {
+  let followMouse = {
+    x: mouseDirection.x,
+    y: mouseDirection.y,
+    w: 10,
+    h: 10,
+  };
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(backGroundImage, 0, 0, canvas.width, canvas.height);
+
   spiderHouse.forEach((spider) => {
-    spider.draw(ctx);
+    if (isCollision(spider, followMouse)) {
+      console.log("collision");
+    }
+    spider.update(ctx);
   });
+
+  ctx.beginPath();
+  ctx.rect(followMouse.x, followMouse.y, followMouse.w, followMouse.h);
+  ctx.fill();
 
   requestAnimationFrame(gameLoop);
 }
 
+function isCollision(box1, box2) {
+  return (
+    box1.x < box2.x + box2.w &&
+    box1.x + box1.w > box2.x &&
+    box1.y < box2.y + box2.h &&
+    box1.y + box1.h > box2.y
+  );
+}
+
 requestAnimationFrame(gameLoop);
+
+canvas.addEventListener("mousemove", (e) => {
+  const boundedArea = canvas.getBoundingClientRect();
+
+  mouseDirection = {
+    x: e.x - boundedArea.left,
+    y: e.y - boundedArea.top,
+  };
+});
--- RandomSpiders/js/spider.js
+++ RandomSpiders/js/spider.js
@@ -1,17 +1,65 @@
+const directions = [-1, 1];
+
 class Spider {
-  constructor(x, y, r) {
+  constructor(x, y, w, h) {
     this.position = {
       x: x || 0,
       y: y || 0,
     };
     this.size = {
-      radius: r || 10,
+      height: h || 10,
+      width: w || 10,
     };
+
+    this.direction = {
+      x: directions[Math.floor(Math.random() * 2)],
+      y: directions[Math.floor(Math.random() * 2)],
+    };
+
+    this.SpiderImage = new Image();
+    this.SpiderImage.src =
+      "https://static.vecteezy.com/system/resources/thumbnails/008/505/759/small/bee-insect-color-illustration-png.png";
   }
   draw(ctx) {
-    ctx.beginPath();
-    ctx.arc(this.position.x, this.position.y, this.size.radius, 0, 2 * Math.PI);
-    ctx.fill();
+    // ctx.beginPath();
+    // ctx.arc(this.position.x, this.position.y, this.size.radius, 0, 2 * Math.PI);
+    // ctx.fill();
+
+    ctx.drawImage(
+      this.SpiderImage,
+      this.position.x,
+      this.position.y,
+      this.size.width,
+      this.size.height
+    );
+  }
+
+  move() {
+    this.position.x = this.position.x + this.direction.x;
+    this.position.y = this.position.y + this.direction.y;
+  }
+
+  collideLeftRight() {
+    if (this.position.x + this.size.width >= canvas.width) {
+      this.direction.x = this.direction.x - 1;
+    } else if (this.position.x <= 0) {
+      this.direction.x = this.direction.x + 1;
+    }
+  }
+
+  collideTopBottom() {
+    if (this.position.y + this.size.height >= canvas.height) {
+      this.direction.y = this.direction.y - 1;
+    } else if (this.position.y <= 0) {
+      this.direction.y = this.direction.y + 1;
+    }
+  }
+
+  update(ctx) {
+    this.draw(ctx);
+    this.move();
+    this.collideLeftRight();
+    this.collideTopBottom();
   }
 }
 
